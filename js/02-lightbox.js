e63a8c82eaac0067050e24eaa35a3f13232af57f
TaraskiN85 <taras12304@i.ua>
2023-10-17 20:03:14 +0300

    accomplish task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryUl = document.querySelector("ul");
+
+const createMarkup = () => {
+  const markup = galleryItems
+    .map(({ description, preview, original }) => {
+      return `<li class="gallery__item">
+              <a class="gallery__link" href=${original}>
+                <img class="gallery__image"
+                  src=${preview} 
+                  alt=${description} 
+                  title=${description}
+                />
+              </a>
+            </li>`;
+    })
+    .join("");
+  galleryUl.insertAdjacentHTML("beforeend", markup);
+};
+
+const openImage = (event) => {
+  event.preventDefault();
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    navText: ["←", "→"],
+  });
+};
+
+createMarkup();
+galleryUl.addEventListener("click", openImage);
